3773afd31b2129ef4d5966228aeb03b62857343e
Niklas Riedinger <20442047+NRiedinger@users.noreply.github.com>
2019-11-24 09:56:24 +0100

    updated button textContent

--- pr0grammToggle.js
+++ pr0grammToggle.js
@@ -6,32 +6,45 @@
 // ==/UserScript==
 
 
-(function() {
+(function () {
     'use strict';
 
     let parentNode = document.getElementById('head-menu');
     let button = document.createElement('a');
+    let url = window.location.href;
+    let key = url.match(/https?:\/\/pr0gramm.com\/(top|new)\/*/)[1];
+
     button.setAttribute('id', 'toggleButtonNewTop');
     button.setAttribute('class', 'head-tab active');
+    button.setAttribute('style', 'margin-left: 15px;');
     button.textContent = 'toggle';
+    if (key === 'new') {
+        button.textContent = 'show top';
+    }
+    else if (key === 'top') {
+        button.textContent = 'show new';
+    }
 
-    if(parentNode){
+    if (parentNode) {
         parentNode.appendChild(button);
 
         document.getElementById('toggleButtonNewTop').addEventListener('click', buttonClickEvent, false);
     }
 })();
 
-function buttonClickEvent(event){
+function buttonClickEvent(event) {
     let url = window.location.href;
     let key = url.match(/https?:\/\/pr0gramm.com\/(top|new)\/*/)[1];
+    let button = document.getElementById('toggleButtonNewTop');
 
-    switch(key){
+    switch (key) {
         case 'new':
             url = url.replace(key, 'top');
+            button.textContent = 'show new';
             break;
         case 'top':
             url = url.replace(key, 'new');
+            button.textContent = 'show top';
             break;
         default:
             break;
